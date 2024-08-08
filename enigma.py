d067a53adad5a9e0facd0ae9bee15c288eefe808
DalBon96 <marcodalbon2@gmail.com>
2024-08-08 15:45:46 +0200

    just try again

--- enigma.py
+++ enigma.py
@@ -4,38 +4,38 @@ class Enigma:
         self.message=message
         self.key=key
 
-    def print_message(self):
-        return f"{self.message}, {self.key} is now sitting."
+    def print_value(self):
+        self.check_key(self)
+        return f"{self.message}, {self.key} these are the values."
+    
+    def check_key(self):
+        while True:
+            try:
+            # Try to convert the input to an integer
+                int(self.key)
+                print("the values is a int")
+                break
+            except ValueError:
+    #       # Handle the case where conversion fails
+                print("That's not a number. Please try again....")
+                self.key = input("Enter a value: ")
 
 #########---END CLASS----------
 
 
 
 #INPUT FUNCTIONS.......
-def menu():
-    message=input("Choose a message...")
-    key=input("Choose the key (1-10)...")
-    while True:
-        try:
-            # Try to convert the input to an integer
-            int(key)
+message=input("Choose a message...")
+key=input("Choose the key (1-10)...")
 
-            #I CALL THE CLASS.......
-            cipher=Enigma(message,key)
-            print(cipher.print_message())
-            ##########################
-
-            break  # Exit the loop if conversion is successful
-        except ValueError:
-            # Handle the case where conversion fails
-            print("That's not a number. Please try again....")
-            key = input("Enter a value: ")
+#I CALL THE CLASS.......
+cipher=Enigma(message,key) 
+print(cipher.print_value())
+##########################
+    
 
     
 
         
-    
-
-menu()
 
         
