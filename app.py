784f40dd658cc8e714124b0c1caef5a8d100f808
DalBon96 <marcodalbon2@gmail.com>
2024-08-28 18:38:10 +0200

    enigma logic 3.1

--- app.py
+++ app.py
@@ -9,16 +9,17 @@ db=mysql.connector.connect(
             user="root",
             password="root",
             database="enigma")
+
 # function to get text from entry widget
 def print_entry_text():
+    
+
     entry_text = entry.get()
     entry_key = key.get()
     while True:
             try:
             # Convert the input to an integer
                 int(entry_key)
-
-            ##########################
                 break  # Exit the loop if conversion is successful
             except Exception as e:
         # Handle the error and display a message box
@@ -28,14 +29,35 @@ def print_entry_text():
     text2 = list(entry_text) #I create the Array
     #new_key=int(key) # i transform the string to int
     ascii_numbers = [ord(char)+int(entry_key) for char in text2] #i transform the values in the list in ASCII and i sum the KEY
-    
         #i transform from ASCII to list of letters
         #and create the edit string
     new_list=[]
     for value in ascii_numbers:
         new_list.append(chr(value))
-    #print(''.join(new_list)) #the new string is edit within a key
     label.config(text=''.join(new_list))
+
+    #I save the value to bring in another method
+    save_value = ''.join(new_list)
+    save_data(entry_text,save_value)
+
+#Method to save the text and the EDIT test  FROM print_entry_text()
+def save_data(text,text2):
+    data=db.cursor()
+    data.execute("INSERT INTO enigma (message,new_message) VALUES (%s,%s)",(text,text2))
+    db.commit()
+
+
+#SECOND OPRTION OF THE MENU
+#SHOW THE LIST
+def print_list():
+    listbox = Tk.Listbox(window)
+    data=db.cursor()
+    data.execute("SELECT * FROM enigma")
+    for x in data:
+    listbox.insert(Tk.END, x)
+    listbox.pack(fill=Tk.BOTH, expand=True)
+    db.commit()
+         
 
 
 
@@ -81,7 +103,7 @@ btn4.pack(pady=10,padx=325) #exit TO THE APP
 
 ####################################################################################################
 
-###################### MENU 1 ######################################################################
+###################### PAGE 1 ######################################################################
  
 title=Label(page1,text="This is page 1",bg="#333333",fg="white")
 title.pack(pady=50,padx=325)
@@ -105,15 +127,18 @@ btn2=Button(page1,text="Come back in MENU",bg="#333333",fg="white",command=lambd
 btn2.pack(pady=50,padx=325)
 
 
-###################### MENU 2 ######################################################################
+###################### PAGE 2 ######################################################################
 
 title=Label(page2,text="This is page 2",bg="#333333",fg="white")
 title.pack(pady=50,padx=325)
 
-btn1=Button(page2,text="Come back in MENU",bg="#333333",fg="white",command=lambda:menu.tkraise())
+btn1=Button(page1,text="Show the Entire list",bg="#333333",fg="white", command=print_list) #The function for print the entire list of items
 btn1.pack(pady=50,padx=325)
 
-###################### MENU 3 ######################################################################
+btn2=Button(page2,text="Come back in MENU",bg="#333333",fg="white",command=lambda:menu.tkraise())
+btn2.pack(pady=50,padx=325)
+
+###################### PAGE 3 ######################################################################
 
 title=Label(page3,text="This is page 3",bg="#333333",fg="white")
 title.pack(pady=50,padx=325)
